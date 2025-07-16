2c900529b4395ed381e2a4bf70a444a1e3be9655
zaved <zavedahmadca707@gmail.com>
2025-07-16 08:05:22 +0530

    Increment app version to 0.0.5
    The `versionCode` has been updated from 1 to 2, and the `versionName` has been updated from "0.0.4" to "0.0.5".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.zavedahmad.yaHabit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.4"
+        versionCode = 2
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
