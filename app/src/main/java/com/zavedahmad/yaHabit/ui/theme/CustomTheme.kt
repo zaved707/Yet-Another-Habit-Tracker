1f0049aefdba7bdfe2a8a4618fb1df8486d5efb2
Zaved <55157101+zaved707@users.noreply.github.com>
2025-07-31 07:28:45 +0530

    merging conflicts

--- app/src/main/java/com/zavedahmad/yaHabit/ui/theme/CustomTheme.kt
+++ app/src/main/java/com/zavedahmad/yaHabit/ui/theme/CustomTheme.kt
@@ -2,8 +2,6 @@ package com.zavedahmad.yaHabit.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -33,7 +31,6 @@ data class OutlineSizes(
 // Define CompositionLocal for outline sizes
 val LocalOutlineSizes = staticCompositionLocalOf { OutlineSizes() }
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun CustomTheme(
     primaryColor: Color? = null,
@@ -94,7 +91,7 @@ fun CustomTheme(
 
     CompositionLocalProvider(LocalOutlineSizes provides OutlineSizes() ) {
 
-    MaterialExpressiveTheme(
+    MaterialTheme(
         colorScheme = colorScheme,
 
 
