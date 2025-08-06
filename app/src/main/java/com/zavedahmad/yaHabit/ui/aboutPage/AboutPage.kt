900897d4e91eb8a8d5997a9c4b7935bfdfb81a73
Zaved <55157101+zaved707@users.noreply.github.com>
2025-08-07 03:10:06 +0530

    cleanup

--- app/src/main/java/com/zavedahmad/yaHabit/ui/aboutPage/AboutPage.kt
+++ app/src/main/java/com/zavedahmad/yaHabit/ui/aboutPage/AboutPage.kt
@@ -1,6 +1,8 @@
 package com.zavedahmad.yaHabit.ui.aboutPage
 
 import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +12,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Gite
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFlexibleTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -26,43 +32,47 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 import androidx.navigation3.runtime.NavKey
 import com.zavedahmad.yaHabit.R
 import com.zavedahmad.yaHabit.ui.components.MyMediumTopABCommon
+import com.zavedahmad.yaHabit.ui.settingsScreen.SettingsItem
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun AboutPage() {
     val scrollBehavior =
-        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+        TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     Scaffold(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {LargeFlexibleTopAppBar(title = { Text("YA Habit Tracker") }, scrollBehavior = scrollBehavior)
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text("About") }, scrollBehavior = scrollBehavior)
 
         }
     )
     { innerPadding ->
         Column(
             Modifier
-                .fillMaxWidth().verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(innerPadding), horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                modifier = Modifier.size(512.dp),
+                modifier = Modifier.size(150.dp),
                 painter = painterResource(id = R.mipmap.ic_launcher_foreground),
                 contentDescription = " Image"
             )
-            Image(
-                modifier = Modifier.size(512.dp),
-                painter = painterResource(id = R.mipmap.ic_launcher_foreground),
-                contentDescription = " Image"
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("YA Habit Tracker", style = MaterialTheme.typography.titleLargeEmphasized)
+                Text(stringResource(R.string.app_version))
+            }
+
+            SettingsItem(
+                icon = Icons.Default.Gite, title = "follow on Github",
+                task = {  },
             )
-            Image(
-                modifier = Modifier.size(512.dp),
-                painter = painterResource(id = R.mipmap.ic_launcher_foreground),
-                contentDescription = " Image"
-            )
+
         }
     }
 }
