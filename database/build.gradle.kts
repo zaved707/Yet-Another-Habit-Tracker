c345e96daf4e47dcbf4c4dc18bad626465c9c771
Zaved <55157101+zaved707@users.noreply.github.com>
2025-08-08 22:43:33 +0530

    fixed a little

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -25,6 +25,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -65,14 +66,14 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
+//    implementation(libs.androidx.ui)
+//    implementation(libs.androidx.ui.graphics)
 
-    
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+//    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
