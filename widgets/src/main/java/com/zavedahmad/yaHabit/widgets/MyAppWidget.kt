931be9d7416b6d81b06d38ca58039358dae413ba
Zaved <55157101+zaved707@users.noreply.github.com>
2025-08-14 01:12:24 +0530

    dependency injection in widget working

--- widgets/src/main/java/com/zavedahmad/yaHabit/widgets/MyAppWidget.kt
+++ widgets/src/main/java/com/zavedahmad/yaHabit/widgets/MyAppWidget.kt
@@ -1,15 +1,47 @@
 package com.zavedahmad.yaHabit.widgets
 
 import android.content.Context
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.glance.Button
 import androidx.glance.GlanceId
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Column
 import androidx.glance.text.Text
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.zavedahmad.yaHabit.database.entities.HabitEntity
+import com.zavedahmad.yaHabit.database.repositories.HabitRepository
+import com.zavedahmad.yaHabit.database.repositories.PreferencesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-class MyAppWidget : GlanceAppWidget(){
+class MyAppWidget(
+    private val habitRepository: HabitRepository,
+    private val preferencesRepository: PreferencesRepository,
+    private val context: Context
+) : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            Text("hello user")
+            GlanceTheme {
+                Scaffold {
+                    val coroutineScope = rememberCoroutineScope()
+                    val habitsState = habitRepository
+                        .getHabitsFlowSortedByIndex()
+                        .collectAsState(emptyList())
+
+                    Column {
+                        habitsState.value.forEach { Text(it.name) }
+
+                    }
+                }
+            }
         }
     }
 }
