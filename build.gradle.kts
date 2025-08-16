55db899f217974eecf6a1ac8fd1af7bce9d3d3ab
Zaved <55157101+zaved707@users.noreply.github.com>
2025-08-16 12:13:41 +0530

    Removed Hilt plugin
    The Hilt plugin has been removed from the project configuration in both `gradle/libs.versions.toml` and `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
 
     id("androidx.room") version roomVersion apply false
-    alias(libs.plugins.hilt) apply false
+
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.android.library) apply false
 }
